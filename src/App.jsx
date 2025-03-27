22ebdc1b4a1485338cef92e537c2294555d6b57c
LukasNunesSS <eidelweinlukas@gmail.com>
2025-03-27 15:40:15 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div>
+      <h1>Simulador RS Schaefer</h1>
+      <p>Projeto configurado corretamente com exportação default.</p>
+    </div>
+  );
+}
